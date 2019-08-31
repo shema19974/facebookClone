b0e1c7933ebc1017b71e04d02235a3a207b11c0a
shema@19974 <musonerwaprince@gmail.com>
2019-08-31 11:37:06 +0000

    Facebook clone, Indentation problem solved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,11 +64,11 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:content, :image, :user_id, :image_cache)
+    params.require(:post).permit(:content, :image, :user_id, :image_cache)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,11 +64,11 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
     end
 end
